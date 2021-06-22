b21ab3dfce67688d1799f03855946dc3604c6ae4
Cjhightower38 <Cjhightower38@gmail.com>
2021-06-22 18:25:25 -0400

    add visuals file for the die

---
+++ die_visual.py
@@ -0,0 +1,12 @@
+from die import Die
+
+# Create a D6
+die = Die()
+
+# Make some rolls, and store results in a list.
+results = []
+for roll_num in range(100):
+	results = die.roll()
+	results.append(result)
+	
+print(results)
