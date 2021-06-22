e2f028884525db056be781c3dcdf3d689bc0d6fb
Cjhightower38 <Cjhightower38@gmail.com>
2021-06-22 18:28:15 -0400

    corrected the spelling of my variable

--- die_visual.py
+++ die_visual.py
@@ -6,7 +6,7 @@ die = Die()
 # Make some rolls, and store results in a list.
 results = []
 for roll_num in range(100):
-	results = die.roll()
+	result = die.roll()
 	results.append(result)
 	
 print(results)
