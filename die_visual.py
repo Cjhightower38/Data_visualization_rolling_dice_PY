6e9761a0adc7834f1d9b5f5d24ee7dedf5abd50e
Cjhightower38 <Cjhightower38@gmail.com>
2021-07-26 14:50:42 -0400

    add plus 1 reminder

--- die_visual.py
+++ die_visual.py
@@ -9,7 +9,11 @@ for roll_num in range(1000):
 	result = die.roll()
 	results.append(result)
 	
-# Analyze the results.
+'''
+Analyze the results. Reminder the +1 is so the program includes
+through all 6 sides of the die off by 1 when you use range.
+'''
+
 frequencies = []
 for value in range (1, die.num_sides + 1):
 	frequency = results.count(value)
